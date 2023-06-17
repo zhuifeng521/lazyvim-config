d28bac69e9ea9ac6807b4e6ce07bfb244c7262ea
zhuifeng521 <1547614915@qq.com>
2023-06-17 11:13:03 +0800

    修改leader键和一些其他快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,7 +3,25 @@
 -- Add any additional keymaps here
 local keymap = vim.keymap
 
-keymap.set("i", "jk", "<Esc>")
+keymap.set("i", "jj", "<Esc>")
+
+-- CTRL SHIFT + 方向 快速跳转
+keymap.set("i", "<C-S-Up>", "<Up><Up><Up><Up><Up><Up><Up>")
+keymap.set("i", "<C-S-Down>", "<Down><Down><Down><Down><Down><Down>")
+keymap.set("i", "<C-S-Left>", "<Home>")
+keymap.set("i", "<C-S-Right>", "<End>")
+keymap.set("n", "<C-S-Up>", "10k")
+keymap.set("n", "<C-S-Down>", "10j")
+keymap.set("n", "<C-S-Left>", "^")
+keymap.set("n", "<C-S-Right>", "$")
+keymap.set("v", "<C-S-Up>", "10k")
+keymap.set("v", "<C-S-Down>", "10j")
+keymap.set("v", "<C-S-Left>", "^")
+keymap.set("v", "<C-S-Right>", "$h")
+-- c-s = :%s/
+
+keymap.set("n", "<A-s>", ":<c-u>%s/\\v//gc<left><left><left><left>")
+keymap.set("v", "<A-s>", ":s/\\v//gc<left><left><left><left>")
 
 -- tab na-- telescope general
 keymap.set("n", "<leader>lds", "<cmd>Telescope lsp_document_symbols<cr>")
