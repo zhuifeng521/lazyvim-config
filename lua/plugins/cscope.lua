e9505609374454234ec7415193281ee2ac70c5ee
zhuifeng521 <1547614915@qq.com>
2023-06-17 20:58:32 +0800

    添加部分快捷键

--- lua/plugins/cscope.lua
+++ lua/plugins/cscope.lua
@@ -11,10 +11,10 @@ return {
     require("cscope_maps").setup({
       disable_maps = false, -- true disables keymaps, only :Cscope will be loaded
       cscope = {
-        -- db_file = "./cscope.out", -- location of cscope db file
+        db_file = "./cscope.out", -- location of cscope db file
         exec = "cscope", -- "cscope" or "gtags-cscope"
         picker = "quickfix", -- "telescope", "fzf-lua" or "quickfix"
-        skip_picker_for_single_result = false, -- jump directly to position for single result
+        skip_picker_for_single_result = true, -- jump directly to position for single result
         db_build_cmd_args = { "-bqkv" }, -- args used for db build (:Cscope build)
       },
     })
