26f412772f4d6246d27b8249a9fb1eb0771cd969
zhuifeng521 <1547614915@qq.com>
2023-07-01 08:01:00 +0800

    添加函数和变量预览插件vsita.vim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,8 +8,9 @@ keymap.set("i", "jj", "<Esc>")
 keymap.set("n", "<leader>ro", ":!cscope -Rkb<cr>")
 keymap.set("n", "<leader>rc", ":!ctags -R .<cr>")
 
-keymap.set("n", "<leader>tc", ":colorscheme tokyonight-storm<cr>")
-
+keymap.set("n", "<leader>tc", ":colorscheme tokyonight-day<cr>")
+-- 快速预览函数和变量定义
+keymap.set("n", "<leader>cp", ":Vista!!<cr>")
 -- 快速切换中文显示
 keymap.set("n", "<leader>dc", ":e ++enc=cp936<cr>")
 
